60b9331e85f66dcdb7dfb1d2dfaf95aa53d5736c
Jeevan <justjeevan07@gmail.com>
2025-11-11 06:04:12 +0530

    feat: add home page component

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="masthead">
+        <h1 className="headline-large">RTI TRANSPARENCY DASHBOARD</h1>
+        <p className="body-text">Tracking India's Right to Information Requests</p>
+      </div>
+    </main>
+  )
+}
